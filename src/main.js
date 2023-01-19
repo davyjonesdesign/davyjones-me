bb8711f188c83e04358c122fe8b3ec184c6d45d8
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2023-01-19 12:44:29 -0600

    parsed to deploy files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Vuelidate from "vuelidate";
-import VueP5 from "vue-p5";
-import VueEasyLightbox from "vue-easy-lightbox";
-
-createApp(App).use(router, Vuelidate, VueEasyLightbox, VueP5).mount("#app");
